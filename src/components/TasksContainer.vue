948bd6314234526468776a48e39ec3e6871b864b
Aryn Sampson <arynsampson0704@gmail.com>
2022-08-13 18:42:24 +0200

    create table to hold task entries

--- src/components/TasksContainer.vue
+++ src/components/TasksContainer.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="tasks-container">
-    <Task
-      v-for="(task, index) in tasks"
-      :task="task.taskInput"
-      :dateInput="task.dateInput"
-      :key="index"
-    />
+    <table>
+      <tr>
+        <th class="task-data">Task</th>
+        <th class="date-data">Due date</th>
+        <th class="tags-data">Tags</th>
+        <th class="action-item">#</th>
+        <th class="action-item">#</th>
+        <th class="action-item">#</th>
+      </tr>
+      <tbody>
+        <Task
+          v-for="(task, index) in tasks"
+          :task="task.taskInput"
+          :dateInput="task.dateInput"
+          :key="index"
+        />
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -24,4 +36,42 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.tasks-container {
+  width: 100%;
+  padding: 15px 30px;
+}
+
+table {
+  width: 100%;
+  text-align: left;
+}
+
+table,
+th,
+td {
+  border: 1px solid rgba(0, 0, 0, 0.5);
+  border-collapse: collapse;
+  padding: 10px;
+}
+
+.task-data {
+  width: 50%;
+}
+
+.date-data {
+  width: 10%;
+}
+
+td.date-data {
+  font-size: 14px;
+}
+
+.tags-data {
+  width: 30%;
+}
+
+.action-items {
+  width: 10%;
+}
+</style>
