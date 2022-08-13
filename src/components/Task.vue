242135ecc56b9f4915bad120c19477f16d13014c
Aryn Sampson <arynsampson0704@gmail.com>
2022-08-13 17:52:55 +0200

    task component that renders items

---
+++ src/components/Task.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>{{ task }}</p>
+    <p>{{ dateInput }}</p>
+  </div>
+  <hr />
+</template>
+
+<script>
+export default {
+  name: 'Task',
+  props: ['task', 'dateInput', 'key'],
+};
+</script>
+
+<style></style>
