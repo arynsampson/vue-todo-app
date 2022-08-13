eea5379fd9a2d572ead71a947727f08c0760b88c
Aryn Sampson <arynsampson0704@gmail.com>
2022-08-13 17:50:11 +0200

    declare data and render form & tasks container component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,46 @@
 <template>
   <div class="home">
-    <Form />
+    <Form
+      addTask="Add task"
+      :modalDisplay="modalDisplay"
+      @newTask="addTaskToArr"
+    />
+    <!-- <div>
+      <button @click="toggleModal">Open modal</button>
+      <Modal v-if="modalDisplay" @close="toggleModal" />
+    </div> -->
   </div>
+
+  <TasksContainer :tasks="tasks" />
 </template>
 
 <script>
 // @ is an alias to /src
-//import HelloWorld from '@/components/HelloWorld.vue'
+import Modal from '@/components/Modal.vue';
 import Form from '@/components/Form.vue';
+import TasksContainer from '@/components/TasksContainer.vue';
 
 export default {
   name: 'Home',
-  components: { Form },
+  components: { Modal, Form, TasksContainer },
+  data() {
+    return {
+      modalDisplay: false,
+      tasks: [],
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.modalDisplay = !this.modalDisplay;
+    },
+    addTaskToArr(task) {
+      this.tasks.push(task);
+    },
+  },
+  watch: {
+    tasks() {
+      console.log(1);
+    },
+  },
 };
 </script>
