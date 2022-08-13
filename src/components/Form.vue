e6f5386068e3d011432595989adf7c546ea11231
Aryn Sampson <arynsampson0704@gmail.com>
2022-08-13 17:49:17 +0200

    intial form with add task logic

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,10 +1,47 @@
 <template>
-  <form><span>Task:</span><input type="text" /></form>
+  <div class="form-wrapper">
+    <form>
+      <div>
+        <label for="task"></label>
+        <input type="text" v-model="taskInput" />
+      </div>
+      <div>
+        <label for="date"></label>
+        <input type="date" v-model="dateInput" />
+      </div>
+
+      <br />
+      <button @click="addNewTask">{{ addTask }}</button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Form',
+  props: ['addTask', 'modalDisplay', 'editTask'],
+  data() {
+    return {
+      taskInput: '',
+      dateInput: '',
+      currentTask: null,
+    };
+  },
+  emits: ['newTask'],
+  methods: {
+    addNewTask(e) {
+      e.preventDefault();
+      this.currentTask = {
+        taskInput: this.taskInput,
+        dateInput: this.dateInput,
+        isCompleted: false,
+      };
+      this.$emit('newTask', this.currentTask);
+      this.taskInput = '';
+      this.dateInput = '';
+      this.currentTask = null;
+    },
+  },
 };
 </script>
 
