651d09f9db31b3e3ee00f891fc0e92a01c4f00b4
Aryn Sampson <arynsampson0704@gmail.com>
2022-08-13 17:52:03 +0200

    tasks container component

---
+++ src/components/TasksContainer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="tasks-container">
+    <Task
+      v-for="(task, index) in tasks"
+      :task="task.taskInput"
+      :dateInput="task.dateInput"
+      :key="index"
+    />
+  </div>
+</template>
+
+<script>
+import Task from './Task.vue';
+
+export default {
+  name: 'TasksContainer',
+  components: {
+    Task,
+  },
+  props: ['tasks'],
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
