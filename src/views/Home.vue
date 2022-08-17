c12bcf3c889f3dc19daa423743e8c43dd72d2982
Aryn Sampson <arynsampson0704@gmail.com>
2022-08-17 16:10:42 +0200

    simplifying the app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,54 +1,45 @@
 <template>
-  <button @click="testAdd">click</button>
-  <div class="home">
-    <Form
-      addTask="Add task"
-      :modalDisplay="modalDisplay"
-      @newTask="addTaskToArr"
-    />
-    <!-- <div>
-      <button @click="toggleModal">Open modal</button>
-      <Modal v-if="modalDisplay" @close="toggleModal" />
-    </div> -->
+  <div class="form-wrapper">
+    <form>
+      <div class="input-wrapper">
+        <label for="task"></label>
+        <input type="text" v-model="taskInput" maxlength="64" required />
+      </div>
+      <button class="btn" @click="addNewTask">Add task</button>
+    </form>
   </div>
 
-  <TasksContainer :tasks="tasks" />
+  <div class="tasks-container">
+    <table>
+      <tr>
+        <th class="task-data">Task</th>
+        <th class="action-item">#</th>
+        <th class="action-item">#</th>
+      </tr>
+      <tbody>
+        <Task
+          v-for="(task, index) in tasks"
+          :task="task.taskInput"
+          :key="index"
+        />
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Modal from '@/components/Modal.vue';
-import Form from '@/components/Form.vue';
-import TasksContainer from '@/components/TasksContainer.vue';
 
 export default {
   name: 'Home',
-  components: { Modal, Form, TasksContainer },
+  components: {},
   data() {
     return {
-      modalDisplay: false,
-      tasks: ['aryn'],
+      tasks: [],
     };
   },
-  methods: {
-    toggleModal() {
-      this.modalDisplay = !this.modalDisplay;
-    },
-    addTaskToArr(task) {
-      this.tasks.push(task);
-    },
-    testAdd() {
-      this.tasks.push('aryn');
-    },
-  },
-  watch: {
-    tasks: {
-      deep: true,
-      handler() {
-        console.log(1);
-      },
-    },
-  },
+  methods: {},
+  mounted() {},
 };
 </script>
 
@@ -56,5 +47,45 @@ export default {
 .home {
   width: 100%;
   margin: 0 auto;
+}
+form {
+  width: 100%;
+}
+.input-wrapper {
+  display: inline-block;
+  margin: 10px;
+}
+input[type='text'] {
+  width: 400px;
+  padding: 15px;
+}
+
+.btn {
+  padding: 15px;
+}
+.tasks-container {
+  width: 100%;
+  margin-top: 2rem;
+}
+
+table {
+  width: 100%;
+  text-align: left;
+}
+
+table,
+th,
+td {
+  border: 1px solid rgba(0, 0, 0, 0.5);
+  border-collapse: collapse;
+  padding: 10px;
+}
+
+.task-data {
+  width: 20%;
+}
+
+.action-item {
+  width: 2%;
 }
 </style>
